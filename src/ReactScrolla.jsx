1ee46a7f77d8c9fb6452814ecd95601ed65fbffd
C.T. Lin <chentsulin@gmail.com>
2015-06-05 01:52:22 +0800

    add jsx to editor config

--- src/ReactScrolla.jsx
+++ src/ReactScrolla.jsx
@@ -5,39 +5,39 @@ import React from 'react'
 
 export default class ReactScrolla extends React.Component {
 
-	constructor(props) {
+  constructor(props) {
     super(props)
     this.state = {}
   }
 
 
-	handleScroll() {
-		var { isLoading, percentage, onPercentage } = this.props
+  handleScroll() {
+    var { isLoading, percentage, onPercentage } = this.props
 
-		if (isLoading) return
+    if (isLoading) return
 
-		var container = React.findDOMNode(this.refs.scrollContainer)
+    var container = React.findDOMNode(this.refs.scrollContainer)
 
-		var { scrollTop, scrollHeight, clientHeight } = container
+    var { scrollTop, scrollHeight, clientHeight } = container
 
-		var percentNow = (scrollTop / (scrollHeight - clientHeight) * 100
+    var percentNow = (scrollTop / (scrollHeight - clientHeight) * 100
 
-		if (onPercentage && (percentNow > percentage)) {
-			onPercentage()
-		}
-	}
+    if (onPercentage && (percentNow > percentage)) {
+      onPercentage()
+    }
+  }
 
-	render() {
-		return (
-			<div
-				onScroll={this.handleScroll}
-				ref="scrollContainer"
-				{...this.props}
-			>
-				{this.props.children}
-			</div>
-		)
-	}
+  render() {
+    return (
+      <div
+        onScroll={this.handleScroll}
+        ref="scrollContainer"
+        {...this.props}
+      >
+        {this.props.children}
+      </div>
+    )
+  }
 }
 
 ReactScrolla.propTypes = {
